4381a7470fdbe24330bca422cf7f651e712f1c54
1999luodi <75122356+1999luodi@users.noreply.github.com>
2024-07-17 15:36:22 +0800

    Add files via upload
    增加划分的固定形式8：1：1

--- maketxt.py
+++ maketxt.py
@@ -13,6 +13,49 @@ train:val:test 比例可以自己调控
 # (train, val): test=0.9:0.1  train:val=0.8:0.2
 
 
+def makeTXT_811(trainpercent,valpercent,testpercent):
+    xmlfilepath = config.ANNOTATION_ROOT  # 获取你源数据的注释路径
+    txtsavepath = config.TARGETROOT  # 设置你txt的保存路径
+    # 递归删除之前存放txt的文件夹，并新建一个
+    try:
+        shutil.rmtree(txtsavepath)
+    except OSError:
+        pass
+    os.makedirs(txtsavepath, exist_ok=True)
+    total_xml = os.listdir(xmlfilepath)  # 获取xml文件列表
+
+    num = len(total_xml)  # 获取xml文件的数量
+    list_indices = list(range(num))  # 获取xml文件的序列号
+    random.shuffle(list_indices)  # 使用shuffle函数打乱列表的次序
+
+    trainp = trainpercent
+    valp = valpercent
+    testp= testpercent
+    tv = int(num * (trainp+valp))  # 划分训练集的数量
+    tr=int(num*trainp)
+    val = int(num * valp)  # 划分验证集的数量
+
+
+    trainval = random.sample(list_indices, tv)
+    train = random.sample(trainval, tr)
+
+    with open(os.path.join(txtsavepath, MODE[0] + '.txt'), 'w') as ftrainval, \
+            open(os.path.join(txtsavepath, MODE[1] + '.txt'), 'w') as ftrain, \
+            open(os.path.join(txtsavepath, MODE[2] + '.txt'), 'w') as fval, \
+            open(os.path.join(txtsavepath, MODE[3] + '.txt'), 'w') as ftest:
+        for i in list_indices:
+            # 获取文件名称中.xml之前的部分
+            name = os.path.splitext(total_xml[i])[0] + '\n'
+
+            if i in trainval:
+                ftrainval.write(name)
+                if i in train:
+                    ftrain.write(name)
+                else:
+                    fval.write(name)
+            else:
+                ftest.write(name)
+
 def maketxt(trainval_percentc, train_trainval_percentc):
     xmlfilepath = config.ANNOTATION_ROOT  # 获取你源数据的注释路径
     txtsavepath = config.TARGETROOT  # 设置你txt的保存路径
